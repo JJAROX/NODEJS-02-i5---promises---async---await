9c5398f28ac54fd497a6db3ae07e15437b018621
Jan Jutaszek <janjuraszek90@gmail.com>
2024-04-09 21:46:49 +0200

    zostalo ostatnie cwiczenie

--- server02.js
+++ server02.js
@@ -1,5 +1,10 @@
 import { totalmem, freemem } from "os"
-
+import { readFile, writeFile, appendFile, readdir } from 'fs';
+import path from 'path';
+const __dirname = path.resolve()
+console.log(freemem());
+console.log(totalmem());
+let logArray = []
 const save = async (repeat) => {
   return new Promise((resolve, reject) => {
     try {
@@ -8,7 +13,30 @@ const save = async (repeat) => {
         number++
         console.log(number);
         console.log(repeat);
+        const date = new Date()
+        let logfile = { totalmem: totalmem(), freemem: freemem() }
+        const filepath = path.join(__dirname, "logs", `log_${date.getFullYear()}_${date.getMonth()}_${date.getDay()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}_${date.getMilliseconds()}.log`)
+        const filepath2 = path.join(__dirname, "logs")
+        appendFile(filepath, JSON.stringify(logfile), (err) => {
+          if (err) throw err
+          console.log("plik utworzony");
+        })
+        if (number === repeat) {
+          clearInterval(interval)
+          readdir(filepath2, (err, files) => {
+            if (err) throw err
+            files.forEach((file) => {
+              readFile(`${filepath2}/${file}`, "utf-8", (err, data) => {
+                if (err) throw err
+                logArray.push(JSON.parse(data))
+                console.log(logArray)
+              })
+            })
+
+          })
+        }
       }, 1000);
+
 
     } catch (error) {
       reject(error.message)
